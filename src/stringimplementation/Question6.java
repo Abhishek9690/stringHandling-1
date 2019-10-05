4b1a5584fdbe7d7eb8273f1d6679d2e1f84c903e
vivek84527 <vivek.singh_cs18@gla.ac.in>
2019-10-05 21:36:42 +0530

    Initial commit

---
+++ src/stringimplementation/Question6.java
@@ -0,0 +1,22 @@
+package stringimplementation;
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String input = sc.nextLine();
+        input=input.toLowerCase();
+        System.out.println("New Lower Case String :"+input);
+
+
+
+
+
+
+
+    }
+
+
+
+}
